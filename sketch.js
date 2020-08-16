9db445cbf515ee75b33a82e914c0e5333b218b7a
Dhruv269 <69427520+Dhruv269@users.noreply.github.com>
2020-08-16 12:21:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var bullet, wall;
+var speed, weight;
+var deformation;
+var thickness;
+
+function setup() {
+  createCanvas(1600, 400);
+  speed=random(223, 321);
+  thickness=random(22, 83);
+  weight=random(30, 52);
+  bullet=createSprite(50, 200, 40, 20);
+  bullet.velocityX=speed; 
+  bullet.shapeColor = "white";
+  wall=createSprite(1200, 200, thickness, height/2);
+  wall.shapeColor = (80, 80, 80);
+}
+
+function draw() {
+  background(0, 0, 0);  
+  
+  if (bullet.x-wall.x<bullet.width/2+wall.width/2
+    &&bullet.x-wall.x<bullet.width/2+wall.width/2
+    &&bullet.y-wall.y<bullet.height/2+wall.height/2
+    &&bullet.y-wall.y<bullet.height/2+wall.height/2) {
+    bullet.shapeColor = "blue"; 
+    wall.shapeColor = "blue";
+   } else {
+    bullet.shapeColor = "red";
+    wall.shapeColor = "red";
+   }
+      
+   if (wall.x-bullet.x < (bullet.width+wall.width)/2) 
+     {
+   bullet.velocityX=0;
+   var deformation=0.5 * weight * speed * speed/22509;
+   if (deformation>180) {
+     bullet.shapeColor=color(255, 0, 0);
+   }
+   if (deformation<180 && deformation>100) {
+     bullet.shapeColor=color(230, 230, 0);
+   }
+   if (deformation<100) {
+     bullet.shapeColor=color(0, 255, 0);
+   }
+   }
+
+   
+  
+
+
+
+    drawSprites();
+}
+
+  
